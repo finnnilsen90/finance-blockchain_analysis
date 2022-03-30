73caec81f3de9ee185ef9cb810fca2bcaf1b06a1
finnnilsen90 <fnilsen1051@gmail.com>
2022-03-29 22:23:15 -0700

    crypto download csv

---
+++ ethereum/beginner_ethereum.py
@@ -0,0 +1,14 @@
+# Intro to Ethereum
+
+# Part 1
+# Link: https://ethereum.org/en/developers/tutorials/a-developers-guide-to-ethereum-part-one/
+
+# Example Blockchain
+{
+   "number": 1234567,
+   "hash": "0xabc123...",
+   "parentHash": "0xdef456...",
+   "miner": "0xa1b2c3...",
+   ...,
+   "transactions": [...]
+}
