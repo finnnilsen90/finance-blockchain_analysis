3d62496ce5916277ebf2cf0d3fc9c4fb2de24350
finnnilsen90 <fnilsen1051@gmail.com>
2022-04-05 22:44:10 -0700

    added functions file and optimized crypto_analysis

---
+++ functions.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+import numpy as np
+from datetime import date
+import json
+
+def fetch_daily_data(symbol):
+    pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/EUR
+    symbol = pair_split[0] + '-' + pair_split[1]
+    url = f'https://api.pro.coinbase.com/products/{symbol}/candles?granularity=86400'
+    response = requests.get(url)
+    today = date.today()
+    get_date = today.strftime("%Y-%m-%d")
+    file = f'data/Coinbase_{pair_split[0] + pair_split[1]}_'+get_date+'.csv'
+    if response.status_code == 200:  # check to make sure the response from server is good
+        data = pd.DataFrame(json.loads(response.text), columns=['unix', 'low', 'high', 'open', 'close', 'volume'])
+        data['date'] = pd.to_datetime(data['unix'], unit='s')  # convert to a readable date
+        data['vol_fiat'] = data['volume'] * data['close']      # multiply the BTC volume by closing price to approximate fiat volume
+        data['ticker'] = symbol
+
+# if we failed to get any data, print an error...otherwise write the file
+        if data is None:
+            print("Did not return any data from Coinbase for this symbol")
+        else:
+            data.to_csv(file, index=False)
+    else:
+        print("Did not receieve OK response from Coinbase API")
+        
+    return file
