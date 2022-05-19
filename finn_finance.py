4749b2c9030aaab1630bbe2e769c87a038f859ce
finnnilsen90 <fnilsen1051@gmail.com>
2022-05-18 18:53:24 -0700

    refactored account_look class

--- finn_finance.py
+++ finn_finance.py
@@ -42,6 +42,67 @@ def candle_stick(data,name):
     data.index.name = 'date'
     mpf.plot(data,type='candle', mav=(10, 20), volume=True, style='charles', title=name)
 
+### ACCOUNT MONITORING
+
+class account_look:
+
+    def __init__(self,transactions,ticker):
+        self.transactions = transactions
+        self.ticker = ticker
+        self.sub_coin = [['BTC/USD',100000000],['ETH/USD',pow(10,18)]]
+
+    def filter_transactions(self):
+
+        transactions = []
+        for i in range(len(self.transactions)):
+            if self.ticker == self.transactions[i][0]:
+                transactions.append(self.transactions[i])
+        
+        return transactions
+
+    def account_holding(self):
+        coin_owned  = 0
+        df = 0
+        today = date.today()
+        get_today = today.strftime("%Y-%m-%d")
+
+        transactions = self.filter_transactions()
+
+        for i in range(len(transactions)):
+            for s in range(len(self.sub_coin)):
+                if self.ticker == self.sub_coin[s][0]:
+                    sub_coin = self.sub_coin[s][1]
+
+            buy_price = transactions[i][1]/sub_coin
+            coin = transactions[i][3]/buy_price
+            coin_owned = coin_owned + coin
+
+            last_day = len(transactions) - 1
+            if i == last_day:
+                transaction_date_start = transactions[i][2]
+                transaction_date_end = get_today
+            else:
+                j = i+1
+                transaction_date_start = transactions[i][2]
+                transaction_date_end = transactions[j][2]
+
+            data_btc = fetch_daily_data(self.ticker)
+            date_value = pd.read_csv(data_btc,index_col=['date'],parse_dates=True,usecols = ['date','close'],chunksize=1000)
+            date_value = pd.concat((x.query("date >= %a and date < %a"%(transaction_date_start,transaction_date_end)) for x in date_value))
+            date_value['value'] = coin_owned*(date_value['close']/sub_coin)
+            date_value = date_value.sort_values(by='date')
+
+            def account_value(close):
+                return coin_owned*(close/sub_coin)
+            pivot = pd.pivot_table(date_value, values=['close'], index=['date'],aggfunc=[account_value])
+            
+            if i == 0:
+                df = pivot
+            else:   
+                df = pd.concat([df,pivot], ignore_index=False)
+                
+        return df
+
 ### BLOCKCHAIN FINANCE ####
 
 def satoshi_price(btc):
@@ -51,30 +112,4 @@ def satoshi_price(btc):
 def wei_price(eth):
     wei = eth/pow(10,18)
     return wei
-
-### OTHER GRAPHS NOT CURRENTLY USED ###
-
-def line_chart(data):
-    series = pd.read_csv(data, usecols=['date','close'])
-    series.sort_values(by = 'date', ascending = True, inplace = True)
-    pyplot.rcParams["figure.figsize"] = [12, 3.50]
-
-    # pyplot.figure(figsize=(50,0))
-    series.set_index('date').plot()
-    # series.show()
-
-def bar_graph(data):
-    date_series = pd.read_csv(data, usecols=['month']).values.tolist()
-    date_series = [date_series[i][0] for i in range(len(date_series))]
-    date_series = date_series[::-1] # reverse date order.
-    high_series  = pd.read_csv(data, usecols=['high']).values.tolist()
-    high_series = [high_series[i][0] for i in range(len(high_series))]
-    close_series  = pd.read_csv(data, usecols=['close']).values.tolist()
-    close_series = [close_series[i][0] for i in range(len(close_series))]
-
-    date_series
-    pyplot.bar(date_series, high_series)
-    pyplot.rcParams["figure.figsize"] = [25, 10]
-    # pyplot.bar(date_series, close_series)
-    pyplot.show()
 
