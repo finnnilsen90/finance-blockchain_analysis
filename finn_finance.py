8d782b30103ed9351da5e258c439c27be32d0f5b
finnnilsen90 <fnilsen1051@gmail.com>
2022-05-22 12:34:52 -0700

    crypto_account refactor

--- finn_finance.py
+++ finn_finance.py
@@ -44,14 +44,14 @@ def candle_stick(data,name):
 
 ### ACCOUNT MONITORING
 
-class account_look:
+class crypto_account:
 
     def __init__(self,transactions,ticker):
         self.transactions = transactions
         self.ticker = ticker
         self.sub_coin = [['BTC/USD',100000000],['ETH/USD',pow(10,18)]]
 
-    def filter_transactions(self):
+    def filter_crypto_transactions(self):
 
         transactions = []
         for i in range(len(self.transactions)):
@@ -60,13 +60,13 @@ class account_look:
         
         return transactions
 
-    def account_holding(self):
+    def crypto_dollar_value(self):
         coin_owned  = 0
         df = 0
         today = date.today()
         get_today = today.strftime("%Y-%m-%d")
 
-        transactions = self.filter_transactions()
+        transactions = self.filter_crypto_transactions()
 
         for i in range(len(transactions)):
             for s in range(len(self.sub_coin)):
@@ -76,7 +76,7 @@ class account_look:
             buy_price = transactions[i][1]/sub_coin
             coin = transactions[i][3]/buy_price
 
-            if 'CREDIT' == transactions[i][4]
+            if 'CREDIT' == transactions[i][4]:
                 coin_owned = coin_owned + coin
             elif 'DEBIT' == transactions[i][4]:
                 coin_owned = coin_owned - coin
@@ -117,3 +117,69 @@ def wei_price(eth):
     wei = eth/pow(10,18)
     return wei
 
+### ACCOUNT PROJECTING ###
+
+# Calculate satoshi bit coin
+
+# def satoshi_price(btc):
+#     satoshi = btc/100000000
+#     return satoshi
+
+# bitcoin_price = [46726.59,38000.00,35000.00,30000.00]
+# bitcoin_sell_price = [70000.00,70000,70000,70000]
+# investment = [100,200,300,300]
+
+# satoshi_owned  = 0
+
+# for i in range(len(investment)):
+#     satoshi_buy_price = satoshi_price(bitcoin_price[i])
+#     satoshi = investment[i]/satoshi_buy_price
+    
+#     satoshi_owned = satoshi_owned + satoshi
+    
+#     print('')
+#     print('Investment '+str(i+1))
+#     print('Satoshi Buy Price: $'+str(satoshi_buy_price))
+#     print('Investment: $'+str(investment[i]))
+#     print('Satoshi: '+str(satoshi))
+
+#     satoshi_sell_price = satoshi_price(bitcoin_sell_price[i])
+#     value = satoshi_sell_price*satoshi
+#     print('Satoshi Sell Price: $'+str(satoshi_sell_price))
+#     print('Value: $'+str(value))
+#     print('Net: $'+str(value-investment[i]))
+
+# def wei_price(eth):
+#     wei = eth/pow(10,18)
+#     return wei
+
+# ethereum_price = [3525.92,2300.00,2100.00]
+# ethereum_sell_price = [4800.00,4800.00,4800.00]
+# investment = [100,300,300]
+
+# for i in range(len(investment)):
+#     ethereum_buy_price = wei_price(ethereum_price[i])
+#     wei = investment[i]/ethereum_buy_price
+    
+#     print('')
+#     print('Investment '+str(i+1))
+#     print('Wei Buy Price: $'+str(ethereum_buy_price))
+#     print('Investment: $'+str(investment[i]))
+#     print('Wei: '+str(wei))
+    
+#     wei_sell_price = wei_price(ethereum_sell_price[i])
+#     value = wei_sell_price*wei
+#     print('Wei Sell Price: $'+str(wei_sell_price))
+#     print('Value: $'+str(value))
+#     print('Net: $'+str(value-investment[i]))
+
+# # Pivot by monthly high and low
+# # Generate BTC CSV.
+# data = f.fetch_daily_data(ticker_crypto[0])
+# df = pd.read_csv(data)
+
+# df['month'] = pd.to_datetime(df['date']).dt.strftime('%B') # make a month column to preserve the order
+# df['year'] = pd.to_datetime(df['date']).dt.strftime('%Y') # make a month column to preserve the order
+# pivot = pd.pivot_table(df, values=['high','low'], index=['month','year','ticker'],aggfunc=np.average)
+# pivot = pivot.sort_values(by=['month','year'], ascending = [True, True])
+# pivot
